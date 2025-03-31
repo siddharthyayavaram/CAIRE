4a65f717b917a279284a6b1fd13716820290162d
siddharthyayavaram <68839837+siddharthyayavaram@users.noreply.github.com>
2025-04-01 02:00:40 +0530

    added check to model_loader

--- src/models/model_loader.py
+++ src/models/model_loader.py
@@ -1,9 +1,45 @@
 import torch
+import logging
 from transformers import (
     Qwen2_5_VLForConditionalGeneration, AutoProcessor,
     MllamaForConditionalGeneration,
     LlavaNextForConditionalGeneration
 )
+
+def is_flash_attn_compatible():
+    if not torch.cuda.is_available():
+        return False
+    
+    try:
+        import flash_attn # type: ignore
+        flash_available = True
+    except ImportError:
+        flash_available = False
+    
+    major = torch.cuda.get_device_properties(0).major
+    return major >= 8 and flash_available # ampere, hopper gpus
+
+USE_FLASH_ATTENTION = is_flash_attn_compatible()
+
+logging.info(f"Using Flash attention: {USE_FLASH_ATTENTION}")
+
+MODEL_CONFIGS = {
+    "qwen_vl": {
+        "model_id": "Qwen/Qwen2.5-VL-7B-Instruct",
+        "model_class": Qwen2_5_VLForConditionalGeneration,
+        "extra_kwargs": {"attn_implementation": "flash_attention_2"} if USE_FLASH_ATTENTION else {}
+    },
+    "llama_vl": {
+        "model_id": "meta-llama/Llama-3.2-11B-Vision-Instruct",
+        "model_class": MllamaForConditionalGeneration,
+        "extra_kwargs": {"attn_implementation": "sdpa"}
+    },
+    "pangea_vl": {
+        "model_id": "neulab/Pangea-7B-hf",
+        "model_class": LlavaNextForConditionalGeneration,
+        "extra_kwargs": {"attn_implementation": "flash_attention_2"} if USE_FLASH_ATTENTION else {}
+    }
+}
 
 MODEL_CONFIGS = {
     "qwen_vl": {
@@ -22,39 +58,6 @@ MODEL_CONFIGS = {
         "extra_kwargs": {"attn_implementation": "flash_attention_2"}
     }
 }
-
-# def is_flash_attn_compatible():
-#     if not torch.cuda.is_available():
-#         return False
-    
-#     try:
-#         import flash_attn # type: ignore
-#         flash_available = True
-#     except ImportError:
-#         flash_available = False
-    
-#     major = torch.cuda.get_device_properties(0).major
-#     return major >= 8 and flash_available # ampere, hopper gpus
-
-# USE_FLASH_ATTENTION = is_flash_attn_compatible()
-
-# MODEL_CONFIGS = {
-#     "qwen_vl": {
-#         "model_id": "Qwen/Qwen2.5-VL-7B-Instruct",
-#         "model_class": Qwen2_5_VLForConditionalGeneration,
-#         "extra_kwargs": {"attn_implementation": "flash_attention_2"} if USE_FLASH_ATTENTION else {}
-#     },
-#     "llama_vl": {
-#         "model_id": "meta-llama/Llama-3.2-11B-Vision-Instruct",
-#         "model_class": MllamaForConditionalGeneration,
-#         "extra_kwargs": {"attn_implementation": "sdpa"}
-#     },
-#     "pangea_vl": {
-#         "model_id": "neulab/Pangea-7B-hf",
-#         "model_class": LlavaNextForConditionalGeneration,
-#         "extra_kwargs": {"attn_implementation": "flash_attention_2"} if USE_FLASH_ATTENTION else {}
-#     }
-# }
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
